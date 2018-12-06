2622f0aa6aa51205e36f888feef6b48162554044
Orann <orann.weber@gmail.com>
2018-12-06 11:04:59 -0500

    added api ucichess

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,5 +1,7 @@
 package project;
 
+import ucichess.ChessBoard;
+import ucichess.UCIChess;
 /**
  *
  * @author Claire, Esther, Orann
@@ -10,7 +12,16 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        UCIChess uci=new UCIChess("/home/orann/Téléchargements/arena_chess/Arena_x86_64_linux");
+        int titou = 2;
+        System.out.println(uci.get_UciOk(false));        
+        System.out.println("Titou");
+        //uci.go_Think(); //calculate white move
+        //make the move
+        System.out.println(uci.get_ReadyOk(false)); //wait for engine be ready
+        uci.move_FromSTART("e2e4", false);
+        //make the move
+
     }
     
 }
